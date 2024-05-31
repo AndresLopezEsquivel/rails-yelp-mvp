89ddd66a92ccc148ee5f54a6209ba6dd88ad59c4
Andres Lopez Esquivel <lopezesquivelandres1@gmail.com>
2024-05-30 21:37:48 -0600

    Seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Restaurant.destroy_all
+
+restaurants = [
+  { name: "Restaurant_1", address: "Address_1", phone_number: "1111", category: "chinese" },
+  { name: "Restaurant_2", address: "Address_2", phone_number: "2222", category: "italian"},
+  { name: "Restaurant_3", address: "Address_3", phone_number: "3333", category: "japanese"},
+  { name: "Restaurant_4", address: "Address_4", phone_number: "4444", category: "french"},
+  { name: "Restaurant_5", address: "Address_5", phone_number: "5555", category: "belgian"}
+]
+
+restaurants.each { |restaurant| Restaurant.create!(restaurant) }
