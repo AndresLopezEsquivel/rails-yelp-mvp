4988aee71a4db443f3a440ed32b3421b5082427d
Andres Lopez Esquivel <lopezesquivelandres1@gmail.com>
2024-06-01 00:01:46 -0600

    Create a new restaurant action

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,9 +6,26 @@ class RestaurantsController < ApplicationController
 
   def show; end
 
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to @restaurant
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :category, :address, :phone_number)
   end
 end
